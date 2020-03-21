73e7625322339733ce2801def27d03cd2d1bc6cd
tu002ndg <tu002ndg@gmail.com>
2020-03-22 02:24:42 +0300

    Home work 5

--- src/hw05/HW05.java
+++ src/hw05/HW05.java
@@ -1,88 +1,83 @@
 package hw05;
 
+import java.util.Arrays;
+
 public class HW05 {
     static final int SIZE = 10000000 ;
     static final int HALF = SIZE / 2 ;
 
-    static class MyThread extends Thread {
-        private float [] arr;
-        private int start;
-
-        public MyThread(float [] arr, int start) {
-            this.arr = arr;
-            this.start = start;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("["+
-                    Thread.currentThread().getName()+
-                    "] : start = "+start);
-            HW05.fill_array(arr, start);
-        }
-
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        float [] arr = new float [SIZE];
-        long ts;
+    public static void main(String[] args) {
         System.out.println("Begin ... ");
         System.out.println("The array length is "+SIZE);
 
-        init_array(arr); // заполняем массив единицами
+        final float [] arr1=initArray(SIZE); // создаем и заполняем массив единицами
+        timing(()->consecutiveExecution(arr1),"Consecutive execution");
 
-        System.out.println("method1 start...");
-        ts = System.currentTimeMillis();
-        method1(arr);
-        System.out.printf("method1 time elapsed: %d(мс)%n", System.currentTimeMillis()-ts);
-
-        init_array(arr);
-
-        System.out.println("method2 start...");
-        ts = System.currentTimeMillis();
-        method2(arr);
-        System.out.printf("method2 time elapsed: %d(мс)%n", System.currentTimeMillis()-ts);
+        final float [] arr2=initArray(SIZE);
+        timing(()->ParallelExecution(arr2),"Parallel execution");
     }
 
-    private static void method1(float arr[]) {
-        fill_array(arr, 0);
+    private static void consecutiveExecution(float arr[]) {
+        fillArray(arr, 0);
     }
 
 
-    private static void method2 (float arr[]) throws InterruptedException {
+    private static void ParallelExecution (float arr[])  {
         float [] arr1 = new float [HALF];
         float [] arr2 = new float [HALF];
 
         System.arraycopy(arr,0,arr1,0,HALF);
         System.arraycopy(arr,HALF,arr2,0,HALF);
 
-        Thread thread1 = new MyThread(arr1, 0);
-        Thread thread2 = new MyThread(arr2, HALF);
+        Thread thread1 = new Thread(()->fillArray(arr1, 0));
+        Thread thread2 = new Thread(()->fillArray(arr2, HALF));
 
         thread1.start();
         thread2.start();
 
-        thread1.join();
-        thread2.join();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
         System.arraycopy(arr1, 0, arr, 0, HALF);
         System.arraycopy(arr2, 0, arr, HALF, HALF);
 
     }
 
-    private static void init_array(float [] arr) {
+    private static float[] initArray(int size) {
         System.out.println("init the array...");
-        for(int i=0;i<arr.length;i++)
-            arr[i]=1;
+        float[] arr = new float[size];
+        Arrays.fill(arr,1);
+        return arr;
     }
 
-    static void fill_array(float [] arr, int start) {
+    static void fillArray(float [] arr, int shift) {
         System.out.println("["+Thread.currentThread().getName()+"]: fill the array...");
-        for(int i=start;i<(arr.length+start);i++) {
-            arr[i-start] = ( float )( arr [ i -start] * Math . sin ( 0.2f + i / 5 ) * Math . cos ( 0.2f + i / 5 ) *
-                    Math . cos ( 0.4f + i / 2 ));
+        for(int i=0;i<(arr.length);i++) {
+            arr[i] = funcValue(arr[i],i+shift);
         }
     }
 
+    private static float funcValue(float idx, int i) {
+        return  ( float )(  idx * Math . sin ( 0.2f + i/5) * Math . cos ( 0.2f + i / 5 ) *
+                Math . cos ( 0.4f + i/ 2 ));
+
+    }
+
+    static void timing(Runnable method, String name) {
+        long ts, tf;
+        System.out.printf("%s start...%n", name);
+        ts = System.currentTimeMillis();
+        method.run();
+        tf = System.currentTimeMillis();
+        System.out.printf("%s time elapsed: %d(мс)%n", name,
+                tf-ts);
+    }
 }
+
+
 
