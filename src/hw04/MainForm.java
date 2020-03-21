d83443d8a04215976e2ba15876deddcf7fc3cc32
tu002ndg <tu002ndg@gmail.com>
2020-03-21 17:53:49 +0300

    Home work 4 updated

--- src/hw04/MainForm.java
+++ src/hw04/MainForm.java
@@ -1,19 +1,15 @@
 package hw04;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MainForm {
     private JPanel pnlMain;
     private JTextField txtMessage;
     private JButton btnSend;
     private JList lstContacts;
-    private JList lstMessages;
+    private JTextArea txtMessageArea;
 
     private final DefaultListModel<String> listModel
-            = new DefaultListModel<>();
-    private final DefaultListModel<String> listModelMsg
             = new DefaultListModel<>();
     private final String[] contacts = {
             "Alex",
@@ -24,39 +20,37 @@ public class MainForm {
             "Patric"
     };
 
-    private final StringBuilder sb = new StringBuilder();
-
     public MainForm() {
         lstContacts.setModel(listModel);
         for (String s:contacts
                 ) {
             listModel.addElement(s);
         }
-        lstContacts.setSelectedIndex(0);
-        lstMessages.setModel(listModelMsg);
-        btnSend.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!txtMessage.getText().isEmpty())
-                listModelMsg.addElement(
-                        "Me> "+txtMessage.getText());
-                txtMessage.setText("");
-                listModelMsg.addElement(
-                        lstContacts.getSelectedValue().toString()
-                                +"> "+"Hello!");
-            }
-        });
+        //lstContacts.setSelectedIndex(0);
+        btnSend.addActionListener(e ->doAction());
+        txtMessage.addActionListener(e ->doAction());
 
     }
 
+    private void doAction() {
+        sendMessage("Me", txtMessage.getText());
+        txtMessage.setText("");
+        if (!lstContacts.isSelectionEmpty())
+        sendMessage(
+                lstContacts.getSelectedValue().toString(),
+                "Hello!");
+    }
 
+    private void sendMessage(String sender, String msg) {
+        if (!msg.trim().isEmpty())
+        txtMessageArea.append(String.format("%s> %s%n",sender,msg));
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Chat ");
         frame.setBounds(300, 100, 400, 500);
         frame.setContentPane(new MainForm().pnlMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.pack();
         frame.setVisible(true);
     }
 }
